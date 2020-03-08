eab177836cdbd956ef91cabf024077d3d3d2edf3
Dhairya Joshi <dhairyajoshi21@gmail.com>
2020-03-08 20:32:26 +0530

    KK
    KK

---
+++ ViewModels/DemoViewModel.cs
@@ -0,0 +1,24 @@
+﻿using MvvmDialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TabsDemo.ViewModels
+{
+    public class DemoViewModel
+    {
+        private readonly IDialogService dialogService;
+        public DemoViewModel()
+        {
+
+        }
+        public void ShowSaveDialog()
+        {
+            //you can code here to show any dialog.
+            MessageBox.Show("Here I want to show save file dialog box for example !");
+        }
+    }
+}
